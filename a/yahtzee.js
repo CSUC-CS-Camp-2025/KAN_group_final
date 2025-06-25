505efbf278753e8d675f842b441cb8c27b89b386
Anthony Cruz <antho.cr04@gmail.com>
2025-06-25 09:39:23 -0700

    fixed issue with duplicates in event listeners

--- a/yahtzee.js
+++ a/yahtzee.js
@@ -7,8 +7,9 @@ let roundNumber = 0;
 //DOM elements
 const btn = document.getElementById('rollButton');
 diceOnBoard = document.getElementsByClassName('dieOnBoard');
+selectedDice = document.getElementsByClassName('selectedDie');
 const boardDiceContainer = document.getElementById('dice-area');
-const selectedDiceContainer = document.getElementById('selected-dice');
+selectedDiceContainer = document.getElementById('selected-dice');
 //Individual Dice
 boardOne = document.getElementById('boardOne');
 boardTwo = document.getElementById('boardTwo');
@@ -20,6 +21,17 @@ selectedTwo = document.getElementById('selectedTwo');
 selectedThree = document.getElementById('selectedThree');
 selectedFour = document.getElementById('selectedFour');
 selectedFive = document.getElementById('selectedFive');
+
+ setELs();
+btn.addEventListener('click', () => {
+    if (roundNumber >= 3) {
+        clearGame();
+    }
+    diceOnBoard = document.getElementsByClassName('dieOnBoard'); //re-init the diceOnBoard
+    dice = []; //clears dice before rolling
+    rollDice();
+    drawDice();
+});
 
 // Generates a random integer between 1 and 6
 function randomizeDie() {
@@ -41,6 +53,7 @@ function getDice() {
 
 //draws dice by reading the dice[] and setting backgroundImages using the diceOnBoard[]
 function drawDice() {
+    //this don't work because it iterates through the loop too many times, based on DiceOnBoard
     for (let i = 0; i < diceOnBoard.length; i++) {
         if (getDice().at(i) == 1) {
             diceOnBoard.item(i).setAttribute('value', 'one');
@@ -64,39 +77,80 @@ function drawDice() {
 }
 
 function setAttributes(element, attributes) {
-  for(var key in attributes) {
-    element.setAttribute(key, attributes[key]);
-  }
-}
-
-testElement = document.createElement('p');
-setAttributes(testElement, {'textColor': 'blue'});
-console.log(testElement);
-
-btn.addEventListener('click', () => {
-    /* TODO: For later implementation
-    if (roundNumber >= 3) {
-        clearGame();
+    for (var key in attributes) {
+        element.setAttribute(key, attributes[key]);
     }
-    */
-    diceOnBoard = document.getElementsByClassName('dieOnBoard'); //re-init the diceOnBoard
-    dice = []; //clears dice before rolling
-    rollDice();
-    drawDice();
-    setBoardELs();
-    console.log(roundNumber);
-});
+}
+
+function checkDuplicates() {
+    let numOnes = 0;
+    let numTwos = 0;
+    let numThrees = 0;
+    let numFours = 0;
+    let numFives = 0;
+    let element;
+    selectedDice = document.getElementsByClassName('selectedDie');
+    for (let i = 0; i < 2; i++) {
+        if (document.getElementById('selectedOne') != null) {
+            numOnes++;
+            console.log('numOnes: ' + numOnes);
+
+        }
+        else if (document.getElementById('selectedTwo') != null) {
+            numTwos++;
+                console.log('numTwos: ' + numTwos);
+
+        }
+        else if (document.getElementById('selectedThree') != null) {
+            numThrees++;
+                console.log('numThrees: ' + numThrees);
+
+        }
+        else if (document.getElementById('selectedFour') != null) {
+            numFours++;
+                console.log('numFours: ' + numFours);
+
+        }
+        else if (document.getElementById('selectedFive') != null) {
+            numFives++;
+                console.log('numFives: ' + numFives);
+
+        }
+    }
+    console.log(selectedDice.length);
+    if (numOnes > 1) {
+        element = document.getElementById('selectedOne');
+        element.remove();
+    }
+    else if (numTwos > 1) {
+        element = document.getElementById('selectedTwo');
+        element.remove();
+    }
+    else if (numThrees > 1) {
+        element = document.getElementById('selectedThree');
+        element.remove();
+    }
+    else if (numFours > 1) {
+        element = document.getElementById('selectedFour');
+        element.remove();
+    }
+    else if (numFives > 1) {
+        element = document.getElementById('selectedFive');
+        element.remove();
+    }
+}
 
 //sets Event Listeners for dice on board
-function setBoardELs() {
+function setELs() {
     boardOne.addEventListener('click', () => {
         let element = document.createElement('div'); //init new div
-        setAttributes(element, {'class': 'selectedDie', 'id': 'selectedOne', 'value': boardOne.getAttribute('value')}); //adds all neccesary attribute to new element
+        setAttributes(element, { 'class': 'selectedDie', 'id': 'selectedOne', 'value': boardOne.getAttribute('value') }); //adds all neccesary selected dice attributes
         selectedDiceContainer.append(element); //adds new element to the selected dice
         selectedOne = document.getElementById('selectedOne'); //re-init selectedOne element
+
         selectedOne.addEventListener('click', () => {
-            let element = document.createElement('div');
-            setAttributes(element, {'class': 'dieOnBoard', 'id': 'boardOne', 'value': selectedOne.getAttribute('value')});
+            let element = document.createElement('div'); //init new div
+            setAttributes(element, { 'class': 'dieOnBoard', 'id': 'boardOne', 'value': selectedOne.getAttribute('value') }); //adds all neccesary board dice attributes
             boardDiceContainer.append(element);
             boardOne = document.getElementById('boardOne');
             selectedOne.remove();
@@ -105,12 +159,13 @@ function setBoardELs() {
     });
     boardTwo.addEventListener('click', () => {
         let element = document.createElement('div');
-        setAttributes(element, {'class': 'selectedDie', 'id': 'selectedTwo', 'value': boardTwo.getAttribute('value')});
+        setAttributes(element, { 'class': 'selectedDie', 'id': 'selectedTwo', 'value': boardTwo.getAttribute('value') });
         selectedDiceContainer.append(element);
         selectedTwo = document.getElementById('selectedTwo');
+
         selectedTwo.addEventListener('click', () => {
             let element = document.createElement('div');
-            setAttributes(element, {'class': 'dieOnBoard', 'id': 'boardTwo', 'value': selectedTwo.getAttribute('value')});
+            setAttributes(element, { 'class': 'dieOnBoard', 'id': 'boardTwo', 'value': selectedTwo.getAttribute('value') });
             boardDiceContainer.append(element);
             boardTwo = document.getElementById('boardTwo');
             selectedTwo.remove();
@@ -119,12 +174,13 @@ function setBoardELs() {
     });
     boardThree.addEventListener('click', () => {
         let element = document.createElement('div');
-        setAttributes(element, {'class': 'selectedDie', 'id': 'selectedThree', 'value': boardThree.getAttribute('value')});
+        setAttributes(element, { 'class': 'selectedDie', 'id': 'selectedThree', 'value': boardThree.getAttribute('value') });
         selectedDiceContainer.append(element);
         selectedThree = document.getElementById('selectedThree');
+
         selectedThree.addEventListener('click', () => {
             let element = document.createElement('div');
-            setAttributes(element, {'class': 'dieOnBoard', 'id': 'boardThree', 'value': selectedThree.getAttribute('value')});
+            setAttributes(element, { 'class': 'dieOnBoard', 'id': 'boardThree', 'value': selectedThree.getAttribute('value') });
             boardDiceContainer.append(element);
             boardThree = document.getElementById('boardThree');
             selectedThree.remove();
@@ -133,12 +189,13 @@ function setBoardELs() {
     });
     boardFour.addEventListener('click', () => {
         let element = document.createElement('div');
-        setAttributes(element, {'class': 'selectedDie', 'id': 'selectedFour', 'value': boardFour.getAttribute('value')});
+        setAttributes(element, { 'class': 'selectedDie', 'id': 'selectedFour', 'value': boardFour.getAttribute('value') });
         selectedDiceContainer.append(element);
         selectedFour = document.getElementById('selectedFour');
+
         selectedFour.addEventListener('click', () => {
             let element = document.createElement('div');
-            setAttributes(element, {'class': 'dieOnBoard', 'id': 'boardFour', 'value': selectedFour.getAttribute('value')});
+            setAttributes(element, { 'class': 'dieOnBoard', 'id': 'boardFour', 'value': selectedFour.getAttribute('value') });
             boardDiceContainer.append(element);
             boardFour = document.getElementById('boardFour');
             selectedFour.remove();
@@ -147,12 +204,13 @@ function setBoardELs() {
     });
     boardFive.addEventListener('click', () => {
         let element = document.createElement('div');
-        setAttributes(element, {'class': 'selectedDie', 'id': 'selectedFive', 'value': boardFive.getAttribute('value')});
+        setAttributes(element, { 'class': 'selectedDie', 'id': 'selectedFive', 'value': boardFive.getAttribute('value') });
         selectedDiceContainer.append(element);
         selectedFive = document.getElementById('selectedFive');
+
         selectedFive.addEventListener('click', () => {
             let element = document.createElement('div');
-            setAttributes(element, {'class': 'dieOnBoard', 'id': 'boardFive', 'value': selectedFive.getAttribute('value')});
+            setAttributes(element, { 'class': 'dieOnBoard', 'id': 'boardFive', 'value': selectedFive.getAttribute('value') });
             boardDiceContainer.append(element);
             boardFive = document.getElementById('boardFive');
             selectedFive.remove();
@@ -161,104 +219,6 @@ function setBoardELs() {
     });
 }
 
-
-/*
-//sets Event Listeners for dice on board
-function setBoardELs() {
-    boardOne.addEventListener('click', () => {
-        let element = document.createElement('div'); //init new div
-        element.setAttribute('class', 'selectedDie'); //sets class to selected dice
-        element.setAttribute('id', 'selectedOne'); //sets id to selected
-        element.setAttribute('value', boardOne.getAttribute('value')); //gets value from die and passes it to new die
-        selectedDiceContainer.append(element); //adds new element to the selected dice
-        selectedOne = document.getElementById('selectedOne'); //re-init selectedOne element
-        selectedOne.addEventListener('click', () => {
-            let element = document.createElement('div');
-            element.setAttribute('class', 'dieOnBoard');
-            element.setAttribute('id', 'boardOne');
-            element.setAttribute('value', selectedOne.getAttribute('value'));
-            boardDiceContainer.append(element);
-            boardOne = document.getElementById('boardOne');
-            selectedOne.remove();
-        });
-        boardOne.remove(); //removes original die
-    });
-    boardTwo.addEventListener('click', () => {
-        let element = document.createElement('div');
-        element.setAttribute('class', 'selectedDie');
-        element.setAttribute('id', 'selectedTwo');
-        element.setAttribute('value', boardTwo.getAttribute('value'));
-        selectedDiceContainer.append(element);
-        selectedTwo = document.getElementById('selectedTwo');
-        selectedTwo.addEventListener('click', () => {
-            let element = document.createElement('div');
-            element.setAttribute('class', 'dieOnBoard');
-            element.setAttribute('id', 'boardTwo');
-            element.setAttribute('value', selectedTwo.getAttribute('value'));
-            boardDiceContainer.append(element);
-            boardTwo = document.getElementById('boardTwo');
-            selectedTwo.remove();
-        });
-        boardTwo.remove();
-    });
-    boardThree.addEventListener('click', () => {
-        let element = document.createElement('div');
-        element.setAttribute('class', 'selectedDie');
-        element.setAttribute('id', 'selectedThree');
-        element.setAttribute('value', boardThree.getAttribute('value'));
-        selectedDiceContainer.append(element);
-        selectedThree = document.getElementById('selectedThree');
-        selectedThree.addEventListener('click', () => {
-            let element = document.createElement('div');
-            element.setAttribute('class', 'dieOnBoard');
-            element.setAttribute('id', 'boardThree');
-            element.setAttribute('value', selectedThree.getAttribute('value'));
-            boardDiceContainer.append(element);
-            boardThree = document.getElementById('boardThree');
-            selectedThree.remove();
-        });
-        boardThree.remove();
-    });
-    boardFour.addEventListener('click', () => {
-        let element = document.createElement('div');
-        element.setAttribute('class', 'selectedDie');
-        element.setAttribute('id', 'selectedFour');
-        element.setAttribute('value', boardFour.getAttribute('value'));
-        selectedDiceContainer.append(element);
-        selectedFour = document.getElementById('selectedFour');
-        selectedFour.addEventListener('click', () => {
-            let element = document.createElement('div');
-            element.setAttribute('class', 'dieOnBoard');
-            element.setAttribute('id', 'boardFour');
-            element.setAttribute('value', selectedFour.getAttribute('value'));
-            boardDiceContainer.append(element);
-            boardFour = document.getElementById('boardFour');
-            selectedFour.remove();
-        });
-        boardFour.remove();
-    });
-    boardFive.addEventListener('click', () => {
-        let element = document.createElement('div');
-        element.setAttribute('class', 'selectedDie');
-        element.setAttribute('id', 'selectedFive');
-        element.setAttribute('value', boardFive.getAttribute('value'));
-        selectedDiceContainer.append(element);
-        selectedFive = document.getElementById('selectedFive');
-        selectedFive.addEventListener('click', () => {
-            let element = document.createElement('div');
-            element.setAttribute('class', 'dieOnBoard');
-            element.setAttribute('id', 'boardFive');
-            element.setAttribute('value', selectedFive.getAttribute('value'));
-            boardDiceContainer.append(element);
-            boardFive = document.getElementById('boardFive');
-            selectedFive.remove();
-        });
-        boardFive.remove();
-    });
-}
-*/
-
-/* TODO: For later implementation
 function clearGame() {
     boardOne.remove();
     boardTwo.remove();
@@ -266,12 +226,19 @@ function clearGame() {
     boardFour.remove();
     boardFive.remove();
     if (selectedOne != null) {
-    selectedOne.remove();
+        selectedOne.remove();
     }
-    selectedTwo.remove();
-    selectedThree.remove();
-    selectedFour.remove();
-    selectedFive.remove();
-    console.log('this method ran');
+    if (selectedTwo != null) {
+        selectedTwo.remove();
+    }
+    if (selectedThree != null) {
+        selectedThree.remove();
+    }
+    if (selectedFour != null) {
+        selectedFour.remove();
+    }
+    if (selectedFive != null) {
+        selectedFive.remove();
+    }
 }
-*/
+
